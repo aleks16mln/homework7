20e2ee6b6fcf76aef60a924636a0df252c5e1566
Aleksey <16mln@mail.ru>
2023-05-30 19:03:04 +0300

    Добавил задачу 47,50,52

---
+++ task47/Program.cs
@@ -0,0 +1,30 @@
+﻿//Задача 47. 
+//Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+//m = 3, n = 4.
+//0,5 7 -2 -0,2
+//1 -3,3 8 -9,9
+//8 7,8 -7,1 9
+Console.Write("Введите количество строк массива и нажмите Enter: ");
+int n = int.Parse(Console.ReadLine());
+
+Console.Write("Введите количество столбцов массиваи нажмите Enter: ");
+int m = int.Parse(Console.ReadLine());
+
+double[,] arr = new double[n, m];
+
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        arr[i, j] = new Random().Next(-99, 99) / 10.0;
+    }
+}
+
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        Console.Write(arr[i, j] + " ");
+    }
+    Console.WriteLine();
+}
---
+++ task50/Program.cs
@@ -0,0 +1,41 @@
+﻿//Задача 50. Напишите программу, которая на вход принимает позиции элемента в 
+ //двумерном массиве, и возвращает 
+ //значение этого элемента или же указание, что такого элемента нет.
+ //Во вводе первая цифра - номер строки, вторая - столбца. Цифры не больше 9.
+ //, задан массив:
+ //1 4 7 2
+ //5 9 2 3
+ //8 4 2 4
+ //17 -> такого числа в массиве нет
+ 
+int rows = ReadInt("Введите индекс строки и нажмите Enter: ");
+int colums = ReadInt("Введите индекс столбца и нажмите Enter: ");
+
+int[,] array = new int[rows, colums];
+
+if (rows < array.GetLength(0) && colums < array.GetLength(1)) 
+Console.WriteLine(array[rows, colums]);
+else Console.WriteLine($"{rows}{colums} -> такого числа в массиве нет");
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = new Random().Next(1, 10);
+    }
+}
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+    
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
